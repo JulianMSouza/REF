a4510f9f4807fd427599cf2ded5695bc3e40050e
Julian <julian.projetos@gmail.com>
2023-02-22 23:28:23 -0300

    Inclusao de componente criado anteriormente.

---
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Ola Vue!!</h1>
+        <p>Meu nome eh  {{  nome }} e trabalho como {{ profissao }}</p>
+    </div>
+   
+</template>
+
+<script>
+    export default {
+        name: 'Primeiro Componente',
+        data(){
+            return {
+                nome: "Matheus",
+                profissao: "Programador"
+            }
+        }
+    }
+</script>
