5ac666d9d66a98e4a08ce634541fffa9944a8402
Julian <julian.projetos@gmail.com>
2023-03-25 11:58:37 -0300

    Inclusao de foto e tratamento para mudar imagem via click no botao.

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,25 +1,33 @@
 <template>
    <div>
     <img :src="avatar" :alt="descricao">    
-    <Reutilizacao />
+    <MudarImagem @mudar-imagem="trocarImagem" />
    </div>
 </template>
 
 <script>
 
 import Reutilizacao  from './Reutilizacao.vue'
+import MudarImagem from './MudarImagem.vue'
 
 export default{
     name: 'Picture',
     components: {
-        Reutilizacao
+        
+        MudarImagem
     },
     data(){
         return{
             avatar: '/img/avatar.png',
             descricao: 'Matheus Batisti'
         }
+    },
+    methods:{
+        trocarImagem(){
+            this.avatar = '/img/avatar2.png'
+        }
     }
+
 }
 
 </script>
