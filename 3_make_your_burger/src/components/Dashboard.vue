d8bb3494992eeca640b12c560720862aa8fc6936
Julian Souza <julian.projetos@gmail.com>
2023-04-25 18:44:27 -0300

    Atualizando pedido atravex da diretiva "@change" dentro do componente select da view. Ao trocar a opcao ele dispara salvando o campo selecionado.

--- 3_make_your_burger/src/components/Dashboard.vue
+++ 3_make_your_burger/src/components/Dashboard.vue
@@ -23,9 +23,9 @@
                     </ul>
                 </div>
                 <div>
-                    <select name="status" class="status">
+                    <select name="status" class="status" @change="updateBurger($event,burger.id)">
                         <option value="">Selecione:</option>
-                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo" >
+                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo" >
                             {{ s.tipo }}
                         </option>
                     </select>
@@ -71,6 +71,25 @@ export default{
           const data =  await req.json();
           //msg
           this.getPedidos();
+
+        },
+        async updateBurger(event,id){
+
+debugger;
+
+          const option = event.target.value;
+
+          const dataJson = JSON.stringify({status: option});
+
+          const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+            method: "PATCH",    ///patch garante que ora atualizar somente o campo enviado.
+            headers: {"Content-Type": "application/json"},
+            body: dataJson
+          });
+
+          const res = await req.json();
+
+          console.log(res);
 
         }
 
